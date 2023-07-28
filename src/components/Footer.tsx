ea5b5e359bbab6b07bbd1e7c716115db07e6609b
Rahul Chauhan <rahul.chauhan@leena.ai>
2023-07-28 12:40:31 +0530

    added navbar and footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+
+const FooterContainer = styled.div`
+  width: 100%;
+  border: 1px solid green;
+`;
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <FooterContainer>Write the code for footer here</FooterContainer>;
 };
 
 export default Footer;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Body from "./Body";
+import styled from "styled-components";
+
+// eslint-disable-next-line react-refresh/only-export-components
+const BodyContainer = styled.div`
+  height: 100vh;
+  min-height: 100vh;
+`;
 
 const layout = () => {
-  return <div>layout</div>;
+  return (
+    <BodyContainer>
+      <Body />
+    </BodyContainer>
+  );
 };
 
 export default layout;
